673de17582053d284ac60dd234a0999524d16a4d
John Trapp <johneyt54@gmail.com>
2016-02-19 11:48:54 -0700

    Fun Time
    WEEE!!!

--- Lab3/Lab3/src/Driver.java
+++ Lab3/Lab3/src/Driver.java
@@ -1,20 +1,32 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * This code is written by John Trapp and Brendan Bellows
+ * for the express purpose to be used in CSCI476 at Montana State University
+ * Spring 2016. Please do not use elsewhere.
  */
+
+import javax.swing.JFrame;
+import java.awt.Image;
+import java.awt.*;
+import javax.swing.*;
 
 /**
  *
- * @author John
+ * @author john.trapp
  */
 public class Driver {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+    //Edit these fields to taylor the driver.
+    private static final String TITLE = "Lab 3: Worm Simulator";
     
+    //Do not modify the code below unless you do not want to include something. 
+    //In which case, you need to comment out.
+    public static void main(String[] args) {
+        JFrame frame = new JFrame(TITLE);
+        frame.setLocation(200, 200);
+        frame.setSize(1000, 1000);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setContentPane(new Lab3Panel());
+        frame.setVisible(true);
+        //frame.pack();
+    }
 }
---
+++ Lab3/Lab3/src/Lab3Panel.java
@@ -0,0 +1,19 @@
+/*
+ * This code is written by John Trapp and Brendan Bellows
+ * for the express purpose to be used in CSCI476 at Montana State University
+ * Spring 2016. Please do not use elsewhere.
+ */
+
+import java.awt.Container;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author John Trapp and Brendan Bellows
+ */
+class Lab3Panel extends JPanel {
+
+    public Lab3Panel() {
+    }
+    
+}
