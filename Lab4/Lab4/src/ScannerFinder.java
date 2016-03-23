8ca2fba27e37fd619d2a5302999defaae9b7918c
John Trapp <johneyt54@gmail.com>
2016-03-22 19:14:43 -0600

    Various Updates

--- Lab4/Lab4/src/ScannerFinder.java
+++ Lab4/Lab4/src/ScannerFinder.java
@@ -1,5 +1,6 @@
 
 import java.util.ArrayList;
+import org.jnetpcap.Pcap;
 
 /*
  * This code is written by John Trapp and Brendan Bellows
@@ -12,37 +13,45 @@ import java.util.ArrayList;
  */
 public class ScannerFinder {
 
-    ArrayList<String> naughtyIPs;
+    public static ArrayList<String> naughtyIPs;
+    public static Pcap pcap;
 
     /**
      * @param args the command line arguments
      */
-    public void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) {
         if (args.length != 1) { //Input checking
             if (args.length == 0) {  //No arguments
-                System.out.println("Please specify a pcap file!");
+                System.err.printf("Please specify a pcap file!\n");
                 System.exit(1);
             } else {  //Too many arguments
-                System.out.println("You entered too many arguments!");
+                System.err.printf("You entered too many arguments!\n");
                 System.exit(1);
             }
         }
 
         openPCAP(args[0]);
-        //Scan for attackers
+        scan();
         printResults();
     }
 
-    public void openPCAP(String inputFile) {
+    public static void openPCAP(String inputFile) {
         //Open PCAP file
+        StringBuilder errbuf = new StringBuilder();
+        pcap = Pcap.openOffline(inputFile, errbuf);
+        if (pcap == null) {
+            System.err.printf("Error while opening file for capture: " + errbuf.toString());
+            System.exit(2);
+        }
     }
 
-    public void scan() {
-
+    public static void scan() {
+        //Scan the PCAP file.
+        //TODO application logic
     }
 
-    public void printResults() {
+    public static void printResults() {
+        //Print the IP's
         for (String naughtyIP : naughtyIPs) {
             System.out.println(naughtyIP);
         }
