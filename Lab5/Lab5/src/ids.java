d4504f589b403cf64034796f6ec803db1e51b7f0
John Trapp <johneyt54@gmail.com>
2016-04-05 15:48:32 -0600

    Add Project

---
+++ Lab5/Lab5/src/ids.java
@@ -0,0 +1,22 @@
+/*
+ * This code is written by John Trapp and Brendan Bellows
+ * for the express purpose to be used in CSCI476 at Montana State University
+ * Spring 2016. Please do not use elsewhere.
+ */
+
+
+
+/**
+ *
+ * @author John Trapp and Brendan Bellows
+ */
+public class ids {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
