0f3d7c85fae56fc89c3f85d6cdad205c4e8d52af
bdbellows <brendan.bellows@gmail.com>
2016-04-19 16:59:25 -0600

    Updated The Things and Stuff
    LOL! This thing now has 34% more stuff!

---
+++ Lab5/Lab5/src/config/ConfigParser.java
@@ -0,0 +1,14 @@
+/*
+ * This program has been written by Brendan Bellows and should
+ * probably be executed with care as he is a student, not a trained
+ * professional. Yet.
+ */
+package config;
+
+/**
+ *
+ * @author Brendan
+ */
+public class ConfigParser {
+    
+}
---
+++ Lab5/Lab5/src/config/Host.java
@@ -0,0 +1,15 @@
+package config;
+
+/*
+ * This program has been written by Brendan Bellows and should
+ * probably be executed with care as he is a student, not a trained
+ * professional. Yet.
+ */
+
+/**
+ *
+ * @author Brendan
+ */
+public class Host {
+    
+}
