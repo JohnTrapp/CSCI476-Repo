c122cfc37135977e705040141b806f987a35a9f3
John Trapp <johneyt54@gmail.com>
2016-04-19 18:57:58 -0600

    Break Update Take 2

--- Lab5/Lab5/src/ids.java
+++ Lab5/Lab5/src/ids.java
@@ -1,10 +1,11 @@
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
-//import org.jnetpcap.Pcap;  
-import org.jnetpcap.packet.PcapPacket;  
-import org.jnetpcap.packet.PcapPacketHandler;  
+import org.jnetpcap.Pcap;
+import org.jnetpcap.packet.PcapPacket;
+import org.jnetpcap.packet.PcapPacketHandler;
 
 /*
  * This code is written by John Trapp and Brendan Bellows
@@ -24,17 +25,16 @@ public class ids {
      */
     public static void main(String[] args) throws IOException {
         /*if(args.length != 2){
-            System.err.println("Incorrect amount of arguments used. "
-                    + "Please correct and try again."
-                    + "\nCorrect Usage: (policy file) (pcap file)");
-            System.exit(0);
-        }*/
+         System.err.println("Incorrect amount of arguments used. "
+         + "Please correct and try again."
+         + "\nCorrect Usage: (policy file) (pcap file)");
+         System.exit(0);
+         }*/
         isInputCorrect(args);
-        
+
         config.ConfigParser parser = new config.ConfigParser();
         //arraylist policys = parse(args[0]);
 
-        
         createPolicies();
         scan(args[1]);
     }
@@ -46,27 +46,38 @@ public class ids {
     private static void scan(String fileIn) {
         final StringBuilder errbuf = new StringBuilder();   //For error messages
         final String file = fileIn;
-        
+
         Pcap pcap = Pcap.openOffline(file, errbuf);
+        if (pcap == null) {
+            System.err.println("Error opening pcap file!\nError from scan class." + errbuf.toString());
+            System.exit(1);
+        }
+        
+        PcapPacketHandler<String> jpacketHandler = new PcapPacketHandler<String>(){
+            @Override
+            public void nextPacket(PcapPacket packet, String user) {
+                
+            }
+        };
     }
 
     private static void isInputCorrect(String[] args) throws IOException {
         boolean policy = true;
-        boolean pcap = true;
-        
-        if(!args[0].contains(".txt")){
+        boolean pcap = new File(args[1]).isFile();
+
+        if (!args[0].contains(".txt")) {
             policy = false;
         }
-        if(!args[1].contains(".pcap")){
+        if (!args[1].contains(".pcap")) {
             pcap = false;
         }
-        
+
         if (!policy || !pcap) { //if not verified
             BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
             System.err.print("Unable to verify files! Please check your arguments. "
                     + "\nVerification Results:"
-                    + "\n\tPolicy file:\t" + policy 
-                    + "\n\tPCAP File:\t" + pcap 
+                    + "\n\tPolicy file:\t" + policy
+                    + "\n\tPCAP File:\t" + pcap
                     + "\nContinue? (Y/N) ");
             String in = reader.readLine();
             if (!in.equalsIgnoreCase("Y")) {
